c98c210fc56d4aa7c4754f5e5f7eaa265c57dd15
Bruno Dahlem <bruno.dahlem2@gmail.com>
2022-07-27 14:36:29 +0200

    Review code

--- src/Form/ConsultantType.php
+++ src/Form/ConsultantType.php
@@ -9,8 +9,6 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ConsultantType extends AbstractType
 {
